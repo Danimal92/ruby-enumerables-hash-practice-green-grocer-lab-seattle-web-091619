12c271bc40cfa9b24f459b0b62a7611609fe174f
Daniel Zaltsman <daniel.zaltsman92@gmail.com>
2019-09-10 23:19:33 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -10,12 +10,17 @@ def consolidate_cart(cart)
       result[key][:count] = 1 
     end
   }} 
+<<<<<<< HEAD
   cart = result
   return cart
+=======
+  return result 
+>>>>>>> dd69eddda61f5a04decb939459f0d8c8db17e966
 end
 
   
   
+<<<<<<< HEAD
 def apply_coupons(cart, coupons) 
   
   coupons.each do |coupon| 
@@ -39,6 +44,46 @@ def apply_coupons(cart, coupons)
 end 
  return cart 
 end
+=======
+
+def apply_coupons(cart, coupons)
+
+  discounted_item = {}
+  cart.each_key{|k|
+    coupons.map{|coupon|
+      key = "#{coupon[:item]} W/COUPON"
+      if k == coupon[:item] and coupon[:num] <= cart[k][:count]       
+        
+                
+        discounted_item[key] = {:price => (coupon[:cost] / ((cart[k][:count]) - ((cart[k][:count]) % (coupon[:num])))) , :clearance => true , :count => (cart[k][:count]) - ((cart[k][:count]) % (coupon[:num]))}        
+        cart[k][:clearance] = true
+        cart[k][:count] = (cart[k][:count]) % (coupon[:num])
+        result = cart.clone
+        result.merge!(discounted_item)
+
+      elsif k == coupon[:item] and coupon[:num] > cart[k][:count]
+        discounted_item = {}
+        key = "#{k} W/COUPON"
+        discounted_item[key][:price] = coupon[:cost] / ((cart[k][:count]) - ((cart[k][:count]) % (coupon[:num])))     
+        discounted_item[key][:clearance] = false
+        discounted_item[key][:count] = 0
+        cart[k][:clearance] = false        
+        result = cart.clone
+        result.merge!(discounted_item)
+      
+      elsif discounted_item.has_key?(key) and coupon[:num] <= result[k][:count]
+        discounted_item[key][:count] += coupon[:count]
+        result[k][:count] = result[k][:count] - coupon[:count]
+        result[k][:price] = (coupon[:cost] / ((result[k][:count])))
+
+      end     
+      return result
+    }
+  }
+    
+  end
+
+>>>>>>> dd69eddda61f5a04decb939459f0d8c8db17e966
 
 def apply_clearance(cart)
   cart.each do |k,v|
